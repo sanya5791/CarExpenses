dd4f672f4f66bdeea509b695f14c06353fc053e3
AKhuronoy <sanya5791@gmail.com>
2018-09-06 16:50:31 +0300

    - show Not 'Implemented' stub  - some UI fixes

--- app/src/main/java/com/carexpenses/akhutornoy/carexpenses/ui/MainActivity.kt
+++ app/src/main/java/com/carexpenses/akhutornoy/carexpenses/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.carexpenses.akhutornoy.carexpenses.ui
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import com.carexpenses.akhutornoy.carexpenses.ui.lpg.LpgFragment
 import com.carexpenses.akhutornoy.carexpenses.R
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,36 +14,61 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         initListeners()
+        onLpgClicked()
     }
 
     private fun initListeners() {
-        navigation.setOnNavigationItemSelectedListener({item ->
+        navigation.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.navigation_lpg -> {
                     onLpgClicked()
                     true
                 }
-                R.id.navigation_dashboard -> {
-                    onDashboardClicked()
+                R.id.navigation_petrol -> {
+                    onPetrolClicked()
                     true
                 }
-                R.id.navigation_notifications -> {
-                    message.setText(R.string.title_notifications)
+                R.id.navigation_service -> {
+                    onServiceClicked()
                     true
                 }
                 else -> false
             }
-        })
+        }
     }
 
     private fun onLpgClicked() {
-        message.setText(R.string.title_lpg)
+        setNotImplementedVisible(false)
         supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, LpgFragment.newInstance())
+                .replace(R.id.fragment_container, LpgFragment.newInstance(), LpgFragment::class.java.simpleName)
                 .commit()
     }
 
-    private fun onDashboardClicked() {
-        message.setText(R.string.title_dashboard)
+    private fun onPetrolClicked() {
+        showNotImplemented()
+    }
+
+    private fun onServiceClicked() {
+        showNotImplemented()
+    }
+
+    private fun showNotImplemented() {
+        setNotImplementedVisible(true)
+        val fragment = supportFragmentManager.findFragmentByTag(LpgFragment::class.java.simpleName)
+
+        fragment ?: return
+
+        supportFragmentManager.beginTransaction()
+                .remove(fragment)
+                .commit()
+    }
+
+    private fun setNotImplementedVisible(isVisible: Boolean) {
+        val visibilityState = if (isVisible) {
+            View.VISIBLE
+        } else {
+            View.GONE
+        }
+        notImplementedTextView.visibility = visibilityState
     }
 }
